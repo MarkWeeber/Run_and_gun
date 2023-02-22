e8f02bd2d13a7f557e141641c033b27fd4bc1b14
MarkWeeber <m.webber@inbox.ru>
2023-02-23 02:46:55 +0600

    adding enemies, zombie models and animations added

---
+++ Assets/Scripts/Avatar/User/PlayerInput.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+namespace Run_n_gun.Space
+{
+    public class PlayerInput : MonoBehaviour
+    {
+        public bool Activated { get { return activated; } set { activated = value; } }
+        private bool activated = false;
+        [SerializeField] private float deadZone = 0.01f;
+        public PlayerMovement PlayerMovement { get { return playerMovement; } set { playerMovement = value; } }
+        private PlayerMovement playerMovement = null;
+        private float horizontalInput = 0f;
+
+        private void Update()
+        {
+            if (activated)
+            {
+                // tracking horizontal inputs
+                horizontalInput = 0f;
+                horizontalInput += Input.GetAxis(GlobalStringVars.HORIZONTAL_AXIS);
+                if (horizontalInput > deadZone || horizontalInput < -deadZone)
+                {
+                    playerMovement.Move(horizontalInput);
+                }
+                // tracking jump button press
+                if (Input.GetButtonDown(GlobalStringVars.JUMP_BUTTON))
+                {
+                    playerMovement.Jump();
+                }
+            }
+            else
+            {
+                horizontalInput = 0f;
+            }
+        }
+    }
+}
